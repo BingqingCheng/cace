a2435e6af4ae40151cd6bf9676fedfdb8fdc8cdc
BingqingCheng <tonicbq@gmail.com>
2023-11-23 14:47:30 +0100

    clean up and add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CACE"',
+    version='0.1.0',
+    author='Bingqing Cheng',
+    author_email='tonicbq@gmail.com',
+    description='Cartesian Atomic Cluster Expansion Machine Learning Potential',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'ase',
+        'PyTorch'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
