7769d3482b70ccebc1784ddc52e13d0905a0ee58
BingqingCheng <tonicbq@gmail.com>
2024-07-04 18:54:06 +0200

    small bug fix

--- cace/data/atomic_data.py
+++ cace/data/atomic_data.py
@@ -136,7 +136,7 @@ class AtomicData(torch_geometric.data.Data):
         if atomic_energies and energy is not None:
             energy -= sum(atomic_energies.get(Z, 0) for Z in atomic_numbers)
         forces = atoms.arrays.get(data_key["forces"], None)  # eV / Ang
-        molecular_index = atoms.info.get(data_key["molecular_index"], None) # index of molecules
+        molecular_index = atoms.arrays.get(data_key["molecular_index"], None) # index of molecules
         stress = atoms.info.get(data_key["stress"], None)  # eV / Ang
         virials = atoms.info.get(data_key["virials"], None)
 
