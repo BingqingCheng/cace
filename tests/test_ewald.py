fcf6bc2af6b10c4a6de748875a82232003400af2
BingqingCheng <tonicbq@gmail.com>
2024-10-05 06:13:06 -0700

    update test_ewald

--- tests/test_ewald.py
+++ tests/test_ewald.py
@@ -2,8 +2,9 @@ import torch
 import cace
 from cace.modules import EwaldPotential
 
-ep = EwaldPotential(dl=3,
-                    sigma=3,
+ep = EwaldPotential(dl=2,
+                    sigma=1,
+                    exponent=1,
                     feature_key='q',
                    aggregation_mode='sum')
 
@@ -26,14 +27,23 @@ def replicate_box(r, q, box, nx=2, ny=2, nz=2):
     new_box = torch.tensor([nx, ny, nz], dtype=r.dtype, device=r.device) * box
     return replicated_r, replicated_q, new_box
 
+# set the same random seed for reproducibility
+torch.manual_seed(0)
 r = torch.rand(100, 3) * 10  # Random positions in a 10x10x10 box
-q = torch.rand(100)  # Random charges
+q = torch.rand(100) * 2 - 1 # Random charges
+
+#q -= torch.mean(q)
 box = torch.tensor([10.0, 10.0, 10.0], dtype=torch.float64)  # Box dimensions
+
+#print(q)
+#exit()
 
 # Replicate the box 2x2x2 times
 replicated_r, replicated_q, new_box = replicate_box(r, q, box, nx=2, ny=2, nz=2)
 
-ew_1 = ep.compute_potential(torch.tensor(r), torch.tensor(q).unsqueeze(1), torch.tensor(box))
-ew_2 = ep.compute_potential(torch.tensor(replicated_r), torch.tensor(replicated_q).unsqueeze(1), torch.tensor(new_box)) / 8
-print(ew_1, ew_2)
+ew_1 = ep.compute_potential_optimized(torch.tensor(r), torch.tensor(q).unsqueeze(1), torch.tensor(box))
+ew_1_s = ep.compute_potential(torch.tensor(r), torch.tensor(q).unsqueeze(1), torch.tensor(box))
+ew_2 = ep.compute_potential_optimized(replicated_r, replicated_q.unsqueeze(1), new_box) / 8
+ew_2_s = ep.compute_potential(replicated_r, replicated_q.unsqueeze(1), new_box) / 8
+print(ew_1, ew_2, ew_1_s, ew_2_s)
 
