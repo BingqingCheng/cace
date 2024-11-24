35ed829b5834c0b1761710c44b81cfd99458cbbe
Daniel King <danielking4812@gmail.com>
2024-11-24 00:37:26 -0800

    new example

--- examples/water_test.py
+++ examples/water_test.py
@@ -1,25 +1,29 @@
 import os
 import glob
 import torch
-from cace.cace.tasks import LightningData, LightningTrainingTask
+from cace.tasks import LightningData, LightningTrainingTask
 
 on_cluster = False
 if 'SLURM_JOB_CPUS_PER_NODE' in os.environ.keys():
     on_cluster = True
 if on_cluster:
-    root = ".../data/water.xyz" #edit paths here
+    root = "/global/scratch/users/king1305/data/water.xyz"
 else:
-    root = ".../cacefit/fit-water/water.xyz" #or here
+    root = "/home/king1305/Apps/cacefit/fit-water/water.xyz"
 
+logs_directory = "lightning_logs"
 logs_name = "water_test"
 cutoff = 5.5
 avge0 = {1: -187.6043857100553, 8: -93.80219285502734}
 batch_size = 4
 data = LightningData(root,batch_size=batch_size,cutoff=cutoff,atomic_energies=avge0)
 
-from cace.cace.representations import Cace
-from cace.cace.modules import BesselRBF, GaussianRBF, GaussianRBFCentered
-from cace.cace.modules import PolynomialCutoff
+training_epochs = 500
+tuning_epochs = 100
+
+from cace.representations import Cace
+from cace.modules import BesselRBF, GaussianRBF, GaussianRBFCentered
+from cace.modules import PolynomialCutoff
 
 #Model
 radial_basis = BesselRBF(cutoff=cutoff, n_rbf=6, trainable=True)
@@ -46,9 +50,9 @@ for batch in data.train_dataloader():
     exdatabatch = batch
     break
 
-from cace.cace.models import NeuralNetworkPotential
-from cace.cace.modules.atomwise import Atomwise
-from cace.cace.modules.forces import Forces
+from cace.models import NeuralNetworkPotential
+from cace.modules.atomwise import Atomwise
+from cace.modules.forces import Forces
 
 atomwise = Atomwise(n_layers=3,
                     output_key="pred_energy",
@@ -66,9 +70,38 @@ model = NeuralNetworkPotential(
     output_modules=[atomwise,forces]
 )
 
-from cace.cace.tasks.lightning import default_losses, default_metrics
-losses = default_losses()
-metrics = default_metrics()
+#Losses
+from cace.tasks import GetLoss
+e_loss = GetLoss(
+    target_name="energy",
+    predict_name='pred_energy',
+    loss_fn=torch.nn.MSELoss(),
+    loss_weight=1,
+)
+f_loss = GetLoss(
+    target_name="force",
+    predict_name='pred_force',
+    loss_fn=torch.nn.MSELoss(),
+    loss_weight=1000,
+)
+losses = [e_loss,f_loss]
+
+#Metrics
+from cace.tools import Metrics
+e_metric = Metrics(
+            target_name="energy",
+            predict_name='pred_energy',
+            name='e',
+            metric_keys=["rmse"],
+            per_atom=True,
+        )
+f_metric = Metrics(
+            target_name="force",
+            predict_name='pred_force',
+            metric_keys=["rmse"],
+            name='f',
+        )
+metrics = [e_metric,f_metric]
 
 #Init lazy layers
 for batch in data.train_dataloader():
@@ -96,8 +129,38 @@ progress_bar = True
 if on_cluster:
     torch.set_float32_matmul_precision('medium')
     progress_bar = False
-task = LightningTrainingTask(model,losses=losses,metrics=metrics,log_rmse=True,
+task = LightningTrainingTask(model,losses=losses,metrics=metrics,
+                             logs_directory="lightning_logs",name=logs_name,
                              scheduler_args={'mode': 'min', 'factor': 0.8, 'patience': 10},
                              optimizer_args={'lr': 0.01},
                             )
-task.fit(data,dev_run=dev_run,max_epochs=500,chkpt=chkpt,name=logs_name, progress_bar=progress_bar)
+task.fit(data,dev_run=dev_run,max_epochs=training_epochs,chkpt=chkpt, progress_bar=progress_bar)
+
+#If you want to do the "fine-tuning" w/higher energy loss:
+if tuning_epochs > 0:
+    e_loss = GetLoss(
+        target_name="energy",
+        predict_name='pred_energy',
+        loss_fn=torch.nn.MSELoss(),
+        loss_weight=1000,
+    )
+    losses = [e_loss,f_loss]
+    
+    #Search for checkpoint
+    latest_version = None
+    num = 0
+    while os.path.isdir(f"lightning_logs/{logs_name}/version_{num}"):
+        latest_version = f"lightning_logs/{logs_name}/version_{num}"
+        num += 1
+    if latest_version:
+        chkpt = glob.glob(f"{latest_version}/checkpoints/*.ckpt")[0]
+    task = LightningTrainingTask(model,losses=losses,metrics=metrics,
+                                 logs_directory="lightning_logs",name=logs_name,
+                                 scheduler_args={'mode': 'min', 'factor': 0.8, 'patience': 10},
+                                 optimizer_args={'lr': 0.01},
+                                )
+    task.fit(data,dev_run=dev_run,max_epochs=training_epochs+tuning_epochs,
+             chkpt=chkpt, progress_bar=progress_bar)
+
+
+
