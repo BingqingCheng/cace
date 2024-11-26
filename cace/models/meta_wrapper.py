e6a1bebfed8aa26c8db8e18e98e50348e5ba246a
Dongjin Kim <djkim@Dongjinui-MacBookAir.local>
2024-11-26 11:49:37 -0800

    meta_wrapper device, dtype specification

--- cace/models/meta_wrapper.py
+++ cace/models/meta_wrapper.py
@@ -1,8 +1,8 @@
 # Recommended metatensor version
 # metatensor                0.2.0
 # metatensor-core           0.1.10
-# metatensor-learn          0.2.0
-# metatensor-operations     0.2.4
+# metatensor-learn          0.3.0
+# metatensor-operations     0.3.0
 # metatensor-torch          0.6.1
 import torch
 import torch.nn as nn
@@ -17,21 +17,29 @@ __all__ = ["MetatensorWrapper"]
 
 # Definition of the MetatensorWrapper class
 class MetatensorWrapper(nn.Module):
-    def __init__(self, model_path: str, atomic_energies: Dict[int, float] = None, device: str = 'cpu'):
+    def __init__(self, 
+        model_path: str, atomic_energies: Dict[int, float] = None, 
+        # device: str = 'cpu', 
+        energy_key: str='CACE_energy', forces_key: str='CACE_forces',
+        cutoff: float = 5.5
+        ):
         super().__init__()
         # Load the pre-trained TorchScript model
-        self.model = torch.jit.load(model_path, map_location=device)
+        self.model = torch.jit.load(model_path)
         self.atomic_energies = atomic_energies
+        self.energy_key = energy_key
+        self.forces_key = forces_key
+        self.cutoff = cutoff
 
     def requested_neighbor_lists(self) -> List[NeighborListOptions]:
         return [
             NeighborListOptions(
-                cutoff=5.5,       # cutoff radius
+                cutoff=self.cutoff,       # cutoff radius
                 full_list=True,   # full neighbor list
                 strict=True,      # strict neighbor list
             ),            
             NeighborListOptions(
-                cutoff=5.5,       # cutoff radius
+                cutoff=self.cutoff,       # cutoff radius
                 full_list=False,   # half neighbor list
                 strict=True,      # strict neighbor list
             )]
@@ -67,20 +75,28 @@ class MetatensorWrapper(nn.Module):
         #get_neighbor_list_option
         neighbor_list_options = self.requested_neighbor_lists()[0]
 
+        #set the device
+        device = systems[0].positions.device
+
         # Iterate over the systems
         for system_index, system in enumerate(systems):
+
             # Extract tensor data from the System object
-            positions = system.positions  # Tensor [n_nodes, 3]
-            atomic_numbers = system.types  # Tensor [n_nodes]
-            cell = system.cell  # Tensor [3, 3]
+            positions = system.positions.to(device)  # Tensor [n_nodes, 3]
+            atomic_numbers = system.types.to(device)  # Tensor [n_nodes]
+            cell = system.cell.to(device)  # Tensor [3, 3]
+
             # Extract data from the neighbor list
             neighbors: TensorBlock = system.get_neighbor_list(neighbor_list_options)
-            edge_index = torch.stack([ neighbors.samples.values[:, 0].to(torch.long),  # first_atom
-                                        neighbors.samples.values[:, 1].to(torch.long)])   # second_atom
-            unit_shifts = neighbors.samples.values[:, 2:5]
+            edge_index = torch.stack([ 
+                neighbors.samples.values[:, 0].to(torch.long).to(device),  # first_atom
+                neighbors.samples.values[:, 1].to(torch.long).to(device)   # second_atom
+                ])   
+            unit_shifts = neighbors.samples.values[:, 2:5].to(device) 
             shifts = torch.matmul(unit_shifts.to(cell.dtype), cell)
+            
             # Batch tensor (all zeros since it's a single system)
-            batch = torch.zeros(atomic_numbers.shape[0], dtype=torch.long)
+            batch = torch.zeros(atomic_numbers.shape[0], dtype=torch.long, device=device)
 
             # Prepare the data dictionary
             data: Dict[str, Tensor] = {
@@ -95,20 +111,21 @@ class MetatensorWrapper(nn.Module):
 
             # Run the model (torch.no_grad() is removed to allow gradient tracking)
             prediction = self.model(data)
+            dtype= prediction[self.energy_key].dtype
 
             if self.atomic_energies:
-                e0 = torch.scalar_tensor(0.0, dtype=torch.float64)
+                e0 = torch.scalar_tensor(0.0, dtype=dtype, device=device)
                 for i in range(len(atomic_numbers)):
                     Z = int(atomic_numbers[i])
-                    e0 += self.atomic_energies.get(Z, 0.0)
+                    e0 += torch.scalar_tensor(self.atomic_energies.get(Z, 0.0), dtype=dtype, device=device)
             else:
-                e0 = torch.scalar_tensor(0.0)
+                e0 = torch.scalar_tensor(0.0, dtype=dtype, device=device)
             
             # Extract energy
-            energy: Tensor = prediction['CACE_energy'] + e0  # Modify to match the model's output key
+            energy: Tensor = prediction[self.energy_key] + e0 
             # make sure no forces is here to solve doulbe backward error
-            if 'CACE_forces' in prediction and prediction['CACE_forces'] is not None:
-                raise ValueError(f"{prediction['CACE_forces'][0]}")
+            if self.forces_key in prediction and prediction[self.forces_key] is not None:
+                raise ValueError(f"{prediction[self.forces_key][0]}")
 
             # Append energy and system index
             energies_list.append(energy.view(1, 1))
@@ -116,28 +133,29 @@ class MetatensorWrapper(nn.Module):
 
         # Stack the energy tensors
         energies: Tensor = torch.cat(energies_list, dim=0)  # Shape: (num_systems, 1)
+
         # Create sample labels
-        samples_values = torch.tensor(samples_list, dtype=torch.int64).unsqueeze(1)
+        samples_values = torch.tensor(samples_list, dtype=torch.int32, device=device).unsqueeze(1)
         samples = Labels(
             names=["system"],
             values=samples_values
         )
         # Create property labels (including CACE_energy)
         properties = Labels(
-            names=["CACE_energy"],  # Add energy name
-            values=torch.tensor([[0]], dtype=torch.int64)  # Single property index
+            names=["energy"],  # the energy must have a single property dimension named "energy"
+            values=torch.tensor([[0]], dtype=torch.int32, device=device)  # with a single entry set to 0.
         )
         # Create a TensorBlock
         energy_block = TensorBlock(
             values=energies,
             samples=samples,
-            components=[],  # Define if needed
+            components=[],  # the energy must not have any components
             properties=properties
         )
         # Create key labels
         keys = Labels(
-            names=["_"],  # Use a key name indicated by metatensor document
-            values=torch.tensor([[0]], dtype=torch.int64)  # [1, 1]
+            names=["_"],  # the energy keys must have a single dimension named "_"
+            values=torch.tensor([[0]], dtype=torch.int32, device=device)  # , with a single entry set to 0
         )
         # Create a TensorMap
         energy_tensormap = TensorMap(
