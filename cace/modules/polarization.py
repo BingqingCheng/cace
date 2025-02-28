a6f8d2f7f3688023d5b4e527238d4b31109fc1ba
dking072 <danielking4812@gmail.com>
2025-02-28 04:26:31 -0600

    addition of quadrupole module

--- cace/modules/polarization.py
+++ cace/modules/polarization.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from typing import Dict
 
-__all__ = ['Polarization', 'Dephase', 'FixedCharge']
+__all__ = ['Polarization', 'Dephase', 'FixedCharge', 'Quadrupole']
 
 class Polarization(nn.Module):
     def __init__(self,
@@ -80,6 +80,56 @@ class Polarization(nn.Module):
         polarization = torch.matmul(box_now.to(S.dtype), 
                                     S.unsqueeze(1)) / (1j * 2.* torch.pi)
         return polarization.reshape(-1), phase
+
+class Quadrupole(nn.Module):
+    #Only real-space implemented
+    def __init__(self,
+                 charge_key: str = 'q',
+                 output_key: str = 'quadrupole',
+                 remove_mean: bool = True,
+                 normalization_factor: float = 1./9.48933,
+                 ):
+        super().__init__()
+        self.charge_key = charge_key
+        self.output_key = output_key
+        self.model_outputs = [output_key]
+        self.remove_mean = remove_mean
+        self.normalization_factor = normalization_factor
+
+    def forward(self, data: Dict[str, torch.Tensor], training=True, output_index=None) -> torch.Tensor:
+
+        if data["batch"] is None:
+            n_nodes = data['positions'].shape[0]
+            batch_now = torch.zeros(n_nodes, dtype=torch.int64, device=data['positions'].device)
+        else:
+            batch_now = data["batch"]
+
+        r = data['positions']
+        q = data[self.charge_key]
+
+        if q.dim() == 1:
+            q = q.unsqueeze(1)
+        if self.remove_mean:
+            q = q - torch.mean(q, dim=0, keepdim=True)
+
+        # Check the input dimension
+        n, d = r.shape
+        assert d == 3, 'r dimension error'
+        assert n == q.size(0), 'q dimension error'
+
+        unique_batches = torch.unique(batch_now)  # Get unique batch indices
+
+        results = []
+        for i in unique_batches:
+            mask = batch_now == i  # Create a mask for the i-th configuration
+            r_now, q_now = r[mask], q[mask]
+            rij = r_now[:,:,None] * r_now[:,None,:]
+            quad = (q_now.reshape(-1,1,1) * rij).sum(dim=0)
+            eye = torch.eye(3).to(rij.device)
+            quad = quad - 1/3*quad.trace()*eye
+            results.append(quad * self.normalization_factor)
+        data[self.output_key] = torch.stack(results, dim=0)
+        return data
 
 class Dephase(nn.Module):
     def __init__(self,
